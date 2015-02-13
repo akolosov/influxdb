b45000c29b789fe12f0d8a62d6835ffac512f660
David Norton <dgnorton@gmail.com>
2015-02-13 13:25:48 -0500

    issue #1430: add where regex to influxql

--- cmd/influxd/server_integration_test.go
+++ cmd/influxd/server_integration_test.go
@@ -166,7 +166,7 @@ func createDatabase(t *testing.T, testName string, nodes cluster, database strin
 	expectedResults := client.Results{
 		Results: []client.Result{
 			{Rows: []influxql.Row{
-				influxql.Row{
+				{
 					Columns: []string{"name"},
 					Values:  [][]interface{}{{"foo"}},
 				},
@@ -312,7 +312,7 @@ func Test_ServerSingleIntegration(t *testing.T) {
 					Name:    "cpu",
 					Columns: []string{"time", "value"},
 					Values: [][]interface{}{
-						[]interface{}{now.Format(time.RFC3339Nano), json.Number("100")},
+						{now.Format(time.RFC3339Nano), json.Number("100")},
 					},
 				}}},
 		},
@@ -361,7 +361,7 @@ func Test_Server3NodeIntegration(t *testing.T) {
 					Name:    "cpu",
 					Columns: []string{"time", "value"},
 					Values: [][]interface{}{
-						[]interface{}{now.Format(time.RFC3339Nano), json.Number("100")},
+						{now.Format(time.RFC3339Nano), json.Number("100")},
 					},
 				}}},
 		},
@@ -411,7 +411,7 @@ func Test_Server5NodeIntegration(t *testing.T) {
 					Name:    "cpu",
 					Columns: []string{"time", "value"},
 					Values: [][]interface{}{
-						[]interface{}{now.Format(time.RFC3339Nano), json.Number("100")},
+						{now.Format(time.RFC3339Nano), json.Number("100")},
 					},
 				}}},
 		},
